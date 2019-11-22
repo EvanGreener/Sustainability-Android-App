04b12959c4887830dc841625bdc71424ae18a02f
seb <sebastien.palin@dawsoncollege.qc.ca>
2019-11-22 14:55:55 -0500

    Created the basic options menu with the three items to go into it (www, about, settings). Inflated it in the main activity and overrode the onItemclick for the menu that currently call blank methods. These functions will create intents to go to the requested activities/website

--- Sustain_CedricSebEvanJean/app/src/main/java/com/sustaincsej/sustain_cedricsebevanjean/activities/MainActivity.kt
+++ Sustain_CedricSebEvanJean/app/src/main/java/com/sustaincsej/sustain_cedricsebevanjean/activities/MainActivity.kt
@@ -2,6 +2,9 @@ package com.sustaincsej.sustain_cedricsebevanjean.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import com.sustaincsej.sustain_cedricsebevanjean.R
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +12,61 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    /**
+     * Method to inflate the options menu
+     */
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu_options, menu)
+        return true
+    }
+
+    /**
+     * Function that will launch one of the options activities when one
+     * of the options are clicked
+     *
+     * @param item
+     * @return
+     */
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.MenuAbout -> {
+                aboutClicked()
+                true
+            }
+            R.id.MenuWWW -> {
+                wwwClicked()
+                true
+            }
+            R.id.MenuSettings -> {
+                settingsClicked()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    /**
+     * Function that will create an intent to go to the About activity
+     */
+    private fun aboutClicked() {
+
+    }
+
+    /**
+     * Function that will create an intent to go to the Dawson Sustain
+     * website.
+     */
+    private fun wwwClicked() {
+
+    }
+
+    /**
+     * Function that will create an intent to go to the Settings activity.
+     */
+    private fun settingsClicked() {
+
     }
 }
