c94a9d94be6946c0183854422d34e661fb28194b
seb <sebastien.palin@dawsoncollege.qc.ca>
2019-11-26 11:00:48 -0500

    Gave the save button in the setting activity have an onclick to a savePreferences function. Thus far, this function will only save the timestamp, as I'm still testing the waters with this. Made it so that the timestamp textview is updated with the new timestamp on save or with the existing one if it already exists

--- Sustain_CedricSebEvanJean/app/src/main/java/com/sustaincsej/sustain_cedricsebevanjean/activities/SettingsActivity.kt
+++ Sustain_CedricSebEvanJean/app/src/main/java/com/sustaincsej/sustain_cedricsebevanjean/activities/SettingsActivity.kt
@@ -3,14 +3,91 @@ package com.sustaincsej.sustain_cedricsebevanjean.activities
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 import com.sustaincsej.sustain_cedricsebevanjean.R
 import java.text.SimpleDateFormat
 import java.util.*
 
+/**
+ * Class that allows the user to input their settings via a form.
+ */
 class SettingsActivity : AppCompatActivity() {
+
+    //All form elements
+    private lateinit var firstName : EditText
+    private lateinit var lastName : EditText
+    private lateinit var email : EditText
+    private lateinit var password : EditText
+    private lateinit var homeLat : EditText
+    private lateinit var homeLon : EditText
+    private lateinit var schoolLat : EditText
+    private lateinit var schoolLon : EditText
+    //To display the date stamp
+    private lateinit var dateStamp : TextView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        getEditTexts()
+        dateStamp = findViewById(R.id.settings_date_stamp)
+
+        updateUI()
     }
+
+    /**
+     * Function that will get all the user input fields and put them into private variables.
+     */
+    private fun getEditTexts() {
+        firstName = findViewById<EditText>(R.id.settings_firstname) as EditText
+        lastName = findViewById<EditText>(R.id.settings_firstname) as EditText
+        email = findViewById<EditText>(R.id.settings_firstname) as EditText
+        password = findViewById<EditText>(R.id.settings_firstname) as EditText
+        homeLat = findViewById<EditText>(R.id.settings_firstname) as EditText
+        homeLon = findViewById<EditText>(R.id.settings_firstname) as EditText
+        schoolLat = findViewById<EditText>(R.id.settings_firstname) as EditText
+        schoolLon = findViewById<EditText>(R.id.settings_firstname) as EditText
+    }
+
+    /**
+     * Function that will save all EditText input to shared preferences
+     */
+    fun savePreferences(view: View) {
+        with(getPreferences(Context.MODE_PRIVATE).edit()) {
+            putString("TimeStamp", getDate())
+            commit()
+        }
+        updateUI()
+    }
+
+    /**
+     * Function that will update the UI when the user saves their settings
+     */
+    private fun updateUI() {
+        val prefs = getPreferences(Context.MODE_PRIVATE)
+        dateStamp.text = prefs.getString("TimeStamp", getString(R.string.settings_last_modified_string))
+    }
+
+    /**
+     * Function that will return today's date. Used for storing the timestamp settings were saved.
+     */
+    private fun getDate() : String {
+        //Today's date
+        val formater = SimpleDateFormat("dd/MM/yyyy")
+        val date = Calendar.getInstance().time
+        return formater.format(date)
+    }
+
+    //Handle back button press
+    override fun onBackPressed() {
+        super.onBackPressed()
+    }
+
+
+
+
 }
