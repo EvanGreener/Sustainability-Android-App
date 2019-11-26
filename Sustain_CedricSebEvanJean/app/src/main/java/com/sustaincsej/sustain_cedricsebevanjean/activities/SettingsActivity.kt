94a3dd8914f2e5f066bd6355729799e2d1bf9c6d
seb <sebastien.palin@dawsoncollege.qc.ca>
2019-11-26 10:01:37 -0500

    Removed some test code that didn't belong

--- Sustain_CedricSebEvanJean/app/src/main/java/com/sustaincsej/sustain_cedricsebevanjean/activities/SettingsActivity.kt
+++ Sustain_CedricSebEvanJean/app/src/main/java/com/sustaincsej/sustain_cedricsebevanjean/activities/SettingsActivity.kt
@@ -1,17 +1,16 @@
 package com.sustaincsej.sustain_cedricsebevanjean.activities
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.sustaincsej.sustain_cedricsebevanjean.R
+import java.text.SimpleDateFormat
+import java.util.*
 
 class SettingsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
-    }
-    //Handle back button press
-    override fun onBackPressed() {
-        super.onBackPressed()
     }
 }
