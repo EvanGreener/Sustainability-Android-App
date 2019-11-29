b906d77116251362cd3ee76d2cf630f3a780a923
Cedric <cedric.richards@dawsoncollege.qc.ca>
2019-11-29 10:59:16 -0500

    Begun the co2 calculator, some functionality, need to get the location from the device

--- Sustain_CedricSebEvanJean/app/src/main/java/com/sustaincsej/sustain_cedricsebevanjean/activities/CO2CalcActivity.kt
+++ Sustain_CedricSebEvanJean/app/src/main/java/com/sustaincsej/sustain_cedricsebevanjean/activities/CO2CalcActivity.kt
@@ -1,20 +1,31 @@
 package com.sustaincsej.sustain_cedricsebevanjean.activities
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Spinner
 import com.sustaincsej.sustain_cedricsebevanjean.R
 import android.widget.ArrayAdapter
-
+import android.widget.TextView
+import com.sustaincsej.sustain_cedricsebevanjean.business.Co2Calculator
 
 
 class CO2CalcActivity : AppCompatActivity() {
+    private lateinit var calculator: Co2Calculator
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_co2_calc)
 
         showSpinner()
+        Log.d("Calculator", "before calc")
+        this.calculator = Co2Calculator(this)
+        calculateDistanceTest()
+    }
+    private fun calculateDistanceTest(){
+        var distanceTest = findViewById<TextView>(R.id.co2calc_totalco2)
+        distanceTest.text = this.calculator.distanceToSchool().toString()
     }
 
     private fun showSpinner() {
