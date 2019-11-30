16a590ecf10992d8c9c7fe71d8f20d09fc9f4069
Cedric <cedric.richards@dawsoncollege.qc.ca>
2019-11-30 14:21:50 -0500

    Implemented more of the calculator, now retreiving the data from the user, and using(partly) the data from shared preferences on what the school and home locations are.

--- Sustain_CedricSebEvanJean/app/src/main/java/com/sustaincsej/sustain_cedricsebevanjean/activities/CO2CalcActivity.kt
+++ Sustain_CedricSebEvanJean/app/src/main/java/com/sustaincsej/sustain_cedricsebevanjean/activities/CO2CalcActivity.kt
@@ -1,32 +1,43 @@
 package com.sustaincsej.sustain_cedricsebevanjean.activities
 
-import android.app.Activity
 import android.content.Context
 import android.location.Location
 import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Spinner
+import android.view.View
+import android.widget.*
 import com.sustaincsej.sustain_cedricsebevanjean.R
-import android.widget.ArrayAdapter
-import android.widget.TextView
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
-import com.sustaincsej.sustain_cedricsebevanjean.business.Co2Calculator
 
 
-class CO2CalcActivity : AppCompatActivity() {
+class CO2CalcActivity : AppCompatActivity() , AdapterView.OnItemSelectedListener {
     private lateinit var calculator: Co2Calculator
+    private lateinit var transportMode : String
+    //if false destination is school
+    private var destinationHome = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_co2_calc)
 
         showSpinner()
+        setUpToggleButton()
         Log.d("Calculator", "before calc")
         this.calculator = Co2Calculator()
         this.calculator.execute()
+    }
+    private fun setUpToggleButton()
+    {
+        val toggle: ToggleButton = findViewById(R.id.co2calc_homeschool_toggle)
+        toggle.setOnCheckedChangeListener { _, isHome ->
+            //if false destination is school
+                destinationHome = isHome
+            Log.i("Calculator", destinationHome.toString())
+        }
+
     }
 
 
@@ -35,6 +46,18 @@ class CO2CalcActivity : AppCompatActivity() {
         val dataAdapter = ArrayAdapter<String>(
             this, R.layout.simple_spinner, resources.getStringArray(R.array.newtrip_travel_modes))
         travelModeSpinner.adapter = dataAdapter
+        travelModeSpinner.onItemSelectedListener = this
+
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+
+        transportMode = parent!!.getItemAtPosition(position) as String
+        Log.i("Calculator", transportMode)
     }
 
     private inner class Co2Calculator : AsyncTask<Void, Float, Float>() {
@@ -42,9 +65,10 @@ class CO2CalcActivity : AppCompatActivity() {
         private lateinit var fusedLocationClient: FusedLocationProviderClient
         private lateinit var currentLocation: Location
         private var haveLocation = false
-        private val dawsonLongitude = -73.58854
-        private val dawsonLatitude = 45.48775
-
+        private var schoolLongitude = -73.58854
+        private var schoolLatutude = 45.48775
+        private var homeLongitude = 0.0
+        private var homeLatutude = 0.0
 
         override fun doInBackground(vararg unused: Void): Float{
             Log.d("Calculator", "initializing calc")
@@ -88,8 +112,8 @@ class CO2CalcActivity : AppCompatActivity() {
                 Location.distanceBetween(
                     this.currentLocation.latitude,
                     this.currentLocation.longitude,
-                    dawsonLatitude,
-                    dawsonLongitude,
+                    schoolLatutude,
+                    schoolLongitude,
                     results
                 )
                 Log.d("Calculator", "distance calc")
@@ -100,7 +124,16 @@ class CO2CalcActivity : AppCompatActivity() {
         }
         override fun onPreExecute() {
             // set up the task here
-
+            with(getSharedPreferences(getString(R.string.Preferences), Context.MODE_PRIVATE).all){
+                var schoolLat = this["SchoolLat"] as String
+                var schoolLon = this["SchoolLon"] as String
+                var homeLat = this["HomeLat"] as String
+                var homeLon = this["HomeLon"] as String
+                schoolLongitude = schoolLon.toDouble()
+                schoolLatutude = schoolLat.toDouble()
+                homeLongitude = homeLon.toDouble()
+                homeLatutude = homeLat.toDouble()
+            }
         }
 
     }
