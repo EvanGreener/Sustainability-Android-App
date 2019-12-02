0b70ff76ef122e1dc4b4ff1a617dc5ddde29706f
seb <sebastien.palin@dawsoncollege.qc.ca>
2019-12-02 12:45:43 -0500

    Testing to see if connecting will work

--- Sustain_CedricSebEvanJean/app/src/main/java/com/sustaincsej/sustain_cedricsebevanjean/activities/SustainabilityFactsActivity.kt
+++ Sustain_CedricSebEvanJean/app/src/main/java/com/sustaincsej/sustain_cedricsebevanjean/activities/SustainabilityFactsActivity.kt
@@ -3,35 +3,39 @@ package com.sustaincsej.sustain_cedricsebevanjean.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.ImageButton
+import android.widget.TextView
 import com.google.firebase.database.*
 import com.sustaincsej.sustain_cedricsebevanjean.R
 import com.sustaincsej.sustain_cedricsebevanjean.models.Fact
+import java.util.*
+import kotlin.collections.ArrayList
 
 class SustainabilityFactsActivity : AppCompatActivity() {
+
+    private lateinit var factImage: ImageButton
+    private lateinit var factText: TextView
+    private lateinit var factSource: TextView
 
     private lateinit var fire: FirebaseDatabase
     private lateinit var database: DatabaseReference
 
-    val factsEventListener = object : ValueEventListener {
+    private val facts: ArrayList<Fact> = ArrayList()
+
+    private val random: Random = Random()
+    private var factsIndex: Int = 0
+
+    private val factsEventListener = object : ValueEventListener {
 
         override fun onDataChange(dataSnapshot: DataSnapshot) {
-            Log.d("TEST", "im here")
-            val thing = dataSnapshot.children.iterator()
-            if (thing.hasNext()) {
-                val index = thing.next()
-                val itemIterator = index.children.iterator()
-
-                while (itemIterator.hasNext()) {
-                    val current = itemIterator.next()
-                    val map = current.value as HashMap<*, *>
-                    val tip = Fact(
-                        current.key!!.toString(),
-                        map["image_url"] as String?,
-                        map["source_url"] as String?,
-                        map["text"] as String?
-                    )
-                    Log.i("TEST", tip.text)
-                }
+            if(dataSnapshot.exists()) {
+                val post = dataSnapshot.getValue(Fact::class.java)
+                facts.add(post!!)
+                Log.i("TEST", post?.text)
+            }
+            else {
+                Log.i("TEST", "nope")
             }
         }
 
@@ -45,17 +49,53 @@ class SustainabilityFactsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sustainability_facts)
 
+        //Get views
+        factImage = findViewById(R.id.sust_fact_image)
+        factText = findViewById(R.id.sust_fact_text)
+        factSource = findViewById(R.id.sust_fact_source)
+
+        //Create database connection and information
         fire = FirebaseDatabase.getInstance()
         Log.d("TEST", fire.app.toString())
         Log.d("TEST", FirebaseDatabase.getSdkVersion())
-        database = FirebaseDatabase.getInstance().reference.child("facts")
+        database = fire.reference.child("facts")
         Log.d("TEST", database.key)
-
-
         database.addValueEventListener(factsEventListener)
+
+        //Display random fact to start
+        randomFact()
     }
 
-    private fun getFacts() {
-
+    /**
+     * Function that is called when the user clicks on the fact image
+     *
+     * @param v The View/image
+     */
+    fun handleFactImageClick(v: View) {
+        nextFact()
     }
+
+    /**
+     * Function that gets a random fact from the firebase database
+     */
+    private fun randomFact() {
+        factsIndex = random.nextInt(6)
+        Log.i("TEST", factsIndex.toString())
+    }
+
+    /**
+     * Function that gets the next fact from firebase.
+     *
+     * Increments to factsIndex by one until it reaches the max size, then resets to zero and restarts.
+     */
+    private fun nextFact() {
+        if (factsIndex == facts.size) {
+            factsIndex = 0
+        }
+        else {
+            factsIndex++
+        }
+        Log.i("TEST", factsIndex.toString())
+    }
+
 }
--- Sustain_CedricSebEvanJean/app/src/main/java/com/sustaincsej/sustain_cedricsebevanjean/models/Fact.kt
+++ Sustain_CedricSebEvanJean/app/src/main/java/com/sustaincsej/sustain_cedricsebevanjean/models/Fact.kt
@@ -1,9 +1,9 @@
 package com.sustaincsej.sustain_cedricsebevanjean.models
 
-import java.io.Serializable
+import com.google.firebase.database.IgnoreExtraProperties
 
+@IgnoreExtraProperties
 data class Fact (
-    var key :String? = "",
     var image_url: String? = "",
     var source_url: String? = "",
     var text: String? = ""
